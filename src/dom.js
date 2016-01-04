3d8a81c9b476fddd388b75d010909bc43012ce4e
Alan R. Soares <alanrsoars@gmail.com>
2016-01-05 11:40:18 +1300

    add prelude-es6 dependency

---
+++ src/dom.js
@@ -0,0 +1,77 @@
+import merge from 'prelude-es6/Obj/merge';
+
+export const $ = (q) => {
+  const els = document.querySelectorAll(q);
+  return els.length > 1 ? els : els[0];
+};
+
+export const append = (e, children) => {
+  children.forEach((child) => e.appendChild(child));
+  return e;
+};
+
+export const replace = (prev, next) =>
+  prev.parentNode.replaceChild(next, prev, next);
+
+export const remove = (e) => e.parentNode.removeChild(e);
+
+export const setAttrs = (el, attrs) => {
+  const set = (key) => {
+    const EVENT = /^on([A-Z]\w+)$/;
+    if (EVENT.test(key))
+      el.addEventListener(key.match(EVENT)[1].toLowerCase(), attrs[key]);
+    else
+      el.setAttribute(key, attrs[key]);
+  };
+
+  Object.keys(attrs).forEach(set);
+
+  return el;
+};
+
+export const h = (tag, attrs = {}, children = []) =>
+  setAttrs(append(document.createElement(tag), children), attrs);
+
+export const t = (text) => document.createTextNode(text);
+
+const isFunction = (x) => typeof x === 'function';
+
+export class Component {
+  constructor(selector) {
+    this.target = null;
+    this.selector = selector;
+    this.state = {};
+  }
+
+  get self() {
+    return $(this.selector);
+  }
+
+  mount(target, props = {}) {
+    this.unmount();
+    this.target = target;
+    this.state = merge({}, props);
+    if (isFunction(this.onMount)) { this.onMount(); }
+    append(this.target, [this.render()]);
+  }
+
+  unmount() {
+    if (isFunction(this.onUnmount)) { this.onUnmount(); }
+    if (this.self) { remove(this.self); }
+  }
+
+  setState(partial = {}) {
+    this.state = merge({}, this.state, partial);
+    this.update();
+  }
+
+  update() {
+    if (isFunction(this.onUpdate)) { this.onUpdate(this.state); }
+
+    const e = this.render();
+
+    if (this.self) {
+      replace(this.self, e);
+    }
+  }
+}
